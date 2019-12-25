2e71a50d2d2024235ee73b9925fbbd6d0507462f
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-12-25 21:38:45 +0200

    deployment

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.get('/api/alert', (request, response) => {
   });
 });
 
-app.use(express.static('/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname + 'build/index.html'));
 });
