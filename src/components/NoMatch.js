4aee3b049bcc029cd090108faaf68bb2a5a2b7c0
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-01-04 14:27:52 +0200

    added new component for 404

---
+++ src/components/NoMatch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './css/no-match.css';
+
+const NoMatch = () => {
+  return (
+    <div className='no-match-container'>
+      <h1 className='no-match-title'>404</h1>
+      <p className='no-match-description'>Sivua ei löytynyt..</p>
+      <NavLink exact to='/'>
+        <div className='no-match-link-wrapper'>
+          <i className='material-icons-round'>keyboard_backspace</i>
+          <p className='no-match-link-back'>Takaisin etusivulle</p>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default NoMatch;
