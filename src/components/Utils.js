97cb9eed4f0edc12de24e5adde12c3d876395051
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-01-04 14:27:06 +0200

    code refactoring

---
+++ src/components/Utils.js
@@ -0,0 +1,99 @@
+import { coordinates } from './Coordinates';
+const locations = [];
+
+export const removeWordsAfterSlash = sentence => {
+  const result = sentence.replace(/\/.*?,/, ',').split(',');
+  return result;
+};
+
+export const removeWordsAfterLastNumber = sentence => {
+  return sentence.replace(/\D+$/g, '');
+};
+
+export const getCoordinates = location => {
+  let lat, long;
+
+  const newLocation = location.split('/')[0];
+  const temp = coordinates.filter(v => v.municipality === newLocation)[0];
+
+  if (!locations.includes(newLocation)) {
+    locations.push(newLocation);
+  }
+
+  // scatter alerts across location
+  if (locations.includes(newLocation)) {
+    lat = temp.latitude + (Math.random() * (10000 + 1)) / 1000000;
+    long = temp.longitude - (Math.random() * (10000 + 1)) / 1000000;
+  } else {
+    lat = temp.latitude;
+    long = temp.longitude;
+  }
+
+  return [lat, long];
+};
+
+export const setAlertType = sentence => {
+  sentence = sentence.toLowerCase();
+
+  if (sentence.includes('hälytys')) {
+    return 'palohälytys';
+  } else if (sentence.includes('palo')) {
+    return 'tulipalo';
+  } else if (sentence.includes('vahingontorjunta')) {
+    return 'vahingontorjunta';
+  } else if (sentence.includes('tie')) {
+    return 'tieliikenneonnettomuus';
+  } else if (sentence.includes('vesi')) {
+    return 'vesiliikenneonnettomuus';
+  } else if (sentence.includes('raide')) {
+    return 'raideliikenneonnettomuus';
+  } else if (sentence.includes('ilma')) {
+    return 'ilmaliikenneonnettomuus';
+  } else if (sentence.includes('ensivaste')) {
+    return 'ensivastetehtävä';
+  } else if (sentence.includes('ihmisen')) {
+    return 'ihmisen pelastaminen';
+  } else if (sentence.includes('eläimen')) {
+    return 'eläimen pelastaminen';
+  } else if (sentence.includes('vaarallisen')) {
+    return 'vaarallisen aineen onnettomuus';
+  } else if (sentence.includes('öljy')) {
+    return 'öljyvahinko';
+  } else {
+    return 'muu';
+  }
+};
+
+export const setIcon = sentence => {
+  if (typeof sentence !== 'string') return 'warning';
+
+  sentence = sentence.toLowerCase();
+
+  if (sentence.includes('hälytys')) {
+    return 'notifications_active';
+  } else if (sentence.includes('palo')) {
+    return 'whatshot';
+  } else if (sentence.includes('vahingontorjunta')) {
+    return 'eco';
+  } else if (sentence.includes('tie')) {
+    return 'directions_car';
+  } else if (sentence.includes('vesi')) {
+    return 'directions_boat';
+  } else if (sentence.includes('raide')) {
+    return 'directions_subway';
+  } else if (sentence.includes('ilma')) {
+    return 'flight';
+  } else if (sentence.includes('ensivaste')) {
+    return 'healing';
+  } else if (sentence.includes('ihmisen')) {
+    return 'emoji_people';
+  } else if (sentence.includes('eläimen')) {
+    return 'pets';
+  } else if (sentence.includes('vaarallisen')) {
+    return 'pan_tool';
+  } else if (sentence.includes('öljy')) {
+    return 'opacity';
+  } else {
+    return 'warning';
+  }
+};
