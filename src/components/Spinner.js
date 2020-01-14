fd6b642e398d9bb70b6a199585bacbfdc04246d1
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-01-14 22:29:02 +0200

    loader shows up earlier when no cache

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import './css/spinner.css';
 
 const Spinner = ({ isLoading }) => {
   return (
-    <div className='loader-container' style={{ visibility: isLoading ? 'visible' : 'hidden' }}>
+    <div className='loader-container' style={{ visibility: isLoading ? null : 'hidden' }}>
       <div className='loader'></div>
       <p className='loader-label'>Ladataan..</p>
     </div>
