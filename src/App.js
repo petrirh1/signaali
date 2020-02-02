d5ed974e5202fe3c2fb591350f1983c527b8c289
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-02-03 00:38:25 +0200

    accessibility tweak

--- src/App.js
+++ src/App.js
@@ -13,89 +13,90 @@ import './App.css';
 const Map = lazy(() => import('./components/Map'));
 
 function App() {
-  const [data, setData] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [hasError, setError] = useState(false);
+	const [data, setData] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [isLoading, setLoading] = useState(true);
+	const [hasError, setError] = useState(false);
 
-  useEffect(() => {
-    ReactGA.initialize('UA-155353804-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+	useEffect(() => {
+		ReactGA.initialize('UA-155353804-1');
+		ReactGA.pageview(window.location.pathname + window.location.search);
 
-    axios
-      .get('/api/alerts')
-      .then(res => {
-        const { item } = res.data.rss.channel[0];
-        const newItem = parse(item);
+		axios
+			.get('/api/alerts')
+			.then(res => {
+				const { item } = res.data.rss.channel[0];
+				const newItem = parse(item);
 
-        setData(newItem);
-        setFiltered(newItem);
-      })
-      .catch(err => {
-        console.log(err);
-        setError(true);
-      })
-      .finally(() => setLoading(false));
-  }, []);
+				setData(newItem);
+				setFiltered(newItem);
+			})
+			.catch(err => {
+				console.log(err);
+				setError(true);
+			})
+			.finally(() => setLoading(false));
+	}, []);
 
-  // outline for accessibility
-  useEffect(() => {
-    const addClass = () => {
-      document.body.classList.add('using-mouse');
-    };
+	// outline for accessibility
+	useEffect(() => {
+		const addClass = () => {
+			document.body.classList.add('using-mouse');
+		};
 
-    const removeClass = () => {
-      document.body.classList.remove('using-mouse');
-    };
+		const removeClass = e => {
+			if (e.key !== 'Tab') return;
+			document.body.classList.remove('using-mouse');
+		};
 
-    document.body.addEventListener('mousedown', addClass);
-    document.body.addEventListener('keydown', removeClass);
+		document.body.addEventListener('mousedown', addClass);
+		document.body.addEventListener('keydown', removeClass);
 
-    return () => {
-      document.body.removeEventListener('mousedown', addClass);
-      document.body.removeEventListener('keydown', removeClass);
-    };
-  }, []);
+		return () => {
+			document.body.removeEventListener('mousedown', addClass);
+			document.body.removeEventListener('keydown', removeClass);
+		};
+	}, []);
 
-  const userFilter = searchTerm => {
-    const newData = [...data];
+	const userFilter = searchTerm => {
+		const newData = [...data];
 
-    const filtered = newData.filter(d =>
-      d.title.toLowerCase().startsWith(searchTerm.toLowerCase())
-    );
-    setFiltered(filtered);
-  };
+		const filtered = newData.filter(d =>
+			d.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+		);
+		setFiltered(filtered);
+	};
 
-  return (
-    <ThemeProvider>
-      <Router>
-        <div className='App'>
-          <Header isLoading={isLoading} hasError={hasError} />
-          <Suspense fallback={<Spinner />}>
-            <Switch>
-              <Route
-                path='/'
-                exact
-                strict
-                render={props => (
-                  <Home
-                    {...props}
-                    userFilter={userFilter}
-                    data={data}
-                    filtered={filtered}
-                    isLoading={isLoading}
-                    hasError={hasError}
-                  />
-                )}
-              />
-              <Route path='/kartta' exact strict render={props => <Map {...props} data={data} />} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Suspense>
-        </div>
-      </Router>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<Router>
+				<div className='App'>
+					<Header isLoading={isLoading} hasError={hasError} />
+					<Suspense fallback={<Spinner />}>
+						<Switch>
+							<Route
+								path='/'
+								exact
+								strict
+								render={props => (
+									<Home
+										{...props}
+										userFilter={userFilter}
+										data={data}
+										filtered={filtered}
+										isLoading={isLoading}
+										hasError={hasError}
+									/>
+								)}
+							/>
+							<Route path='/kartta' exact strict render={props => <Map {...props} data={data} />} />
+							<Route component={NoMatch} />
+						</Switch>
+					</Suspense>
+				</div>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
