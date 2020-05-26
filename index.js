f20e4373eda2930fd0335de0c291d1c34a7f1731
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-05-26 14:32:20 +0300

    added compression

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const compression = require('compression');
 const PORT = process.env.PORT || 5000;
-const axios = require('axios');
 const path = require('path');
 const app = express();
 const http = require('http');
@@ -11,26 +11,27 @@ const url = 'http://www.peto-media.fi/tiedotteet/rss.xml';
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/api/alerts', (request, response) => {
-  let req = http.get(url, res => {
-    let data = '';
-    res.setEncoding('latin1');
-    res.on('data', stream => {
-      data += stream;
-    });
-    res.on('end', () => {
-      parser.parseString(data, (err, res) => {
-        if (err === null) {
-          response.send(res);
-        } else {
-          console.log(err);
-        }
-      });
-    });
-  });
+	let req = http.get(url, res => {
+		let data = '';
+		res.setEncoding('latin1');
+		res.on('data', stream => {
+			data += stream;
+		});
+		res.on('end', () => {
+			parser.parseString(data, (err, res) => {
+				if (err === null) {
+					response.send(res);
+				} else {
+					console.log(err);
+				}
+			});
+		});
+	});
 });
 
+app.use(compression);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+	res.sendFile(path.join(__dirname + '/build/index.html'));
 });
